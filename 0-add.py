3fd6e58fdb71dac6e7e12072ec0e70160a83dd00
richardrapando <122984478+richardrapando@users.noreply.github.com>
2024-03-14 08:55:00 +0300

    Python - Variable Annotations

---
+++ 0-add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+'''type-annotated function add
+'''
+
+
+def add(a: float, b: float) -> float:
+    '''takes float a and float b as arguments and returns their sum as a float.
+    '''
+    return a + b
