79a26325aba4f6184fc75307cacc877c775004b4
richardrapando <122984478+richardrapando@users.noreply.github.com>
2024-03-14 09:09:20 +0300

    Python - Variable Annotations

---
+++ 6-sum_mixed_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+'''type-annotated function sum_mixed_list
+'''
+from typing import List, Union
+
+
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
+    '''takes list mxd_lst of integers and floats and returns their sum as a float.
+    '''
+    return float(sum(mxd_lst))
