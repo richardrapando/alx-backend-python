4c93046fd93f323150a7fa7c72a294e65d1e57ad
richardrapando <122984478+richardrapando@users.noreply.github.com>
2024-03-14 09:10:44 +0300

    Python - Variable Annotations

---
+++ 7-to_kv.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+'''type-annotated function to_kv
+'''
+from typing import Union, Tuple
+
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    '''takes a string k and an int OR float v as arguments and returns a tuple
+    '''
+    return (k, float(v**2))
