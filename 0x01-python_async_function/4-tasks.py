c9144082c264b5d6e26db1e1fa738610d6c29ca4
richardrapando <122984478+richardrapando@users.noreply.github.com>
2024-03-18 13:45:11 +0300

    Python - Async

---
+++ 0x01-python_async_function/4-tasks.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+'''The basics of async
+'''
+import asyncio
+from typing import List
+
+
+task_wait_random = __import__('3-tasks').task_wait_random
+
+
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    '''task_wait_random is being called.
+    '''
+    wait_times = await asyncio.gather(
+        *tuple(map(lambda _: task_wait_random(max_delay), range(n)))
+    )
+    return sorted(wait_times)
